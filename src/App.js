be9f772d9b417f54e6cd514572decaf86d7cb498
rusiruavb <rusiru.bandara@arimaclanka.com>
2023-05-05 03:24:55 +0530

    add redux configs

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { createBrowserHistory } from 'history';
+import { Router, Route, Switch } from 'react-router-dom';
 
-import Navbar from "./components/navbar.component";
+import Navbar from './components/navbar.component';
 import ProductList from './components/product-list.component';
 import AddProduct from './components/product-add.component';
 import EditProduct from './components/product-edit.component';
+
+export const history: any = createBrowserHistory();
 
 function App() {
   return (
     <div>
       <Navbar />
-      <Router>
-        <Routes>
-          <Route exact path="/product" element={<ProductList />} />
-          <Route exact path="/createproduct" element={<AddProduct />} />
-          <Route exact path="/editproduct" element={<EditProduct />} />
-        </Routes>
+      <Router history={history}>
+        <Switch>
+          <Route path="/createproduct" exact>
+            <AddProduct />
+          </Route>
+          <Route path="/editproduct" exact>
+            <EditProduct />
+          </Route>
+          <Route path="/" exact>
+            <ProductList />
+          </Route>
+        </Switch>
       </Router>
     </div>
   );
-
 }
 
 export default App;
