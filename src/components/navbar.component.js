f00d606b55329f8c3b589ea29c9fdf9ed73c72ea
kalanarathnayake <kalanagayanga8@gmail.com>
2023-05-04 20:25:11 +0530

    fix

--- src/components/navbar.component.js
+++ src/components/navbar.component.js
@@ -1,3 +1,11 @@
+import { Fragment } from 'react';
+import { Menu, Transition } from '@headlessui/react';
+import { ChevronDownIcon } from '@heroicons/react/20/solid';
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(' ')
+}
+
 function navbar() {
   return (
     <div>
@@ -5,14 +13,66 @@ function navbar() {
         <div className="mb-2 sm:mb-0">
         </div>
         <div className='text-lg font-light hover:text-blue-dark'>
-          <a href="/product" className="m-2 text-black no-underline duration-300 hover:text-blue-800 hover:font-normal">Group Chats</a>
+          <a href="/product" className="m-2 text-black no-underline duration-300 hover:text-blue-800 hover:font-normal">Products</a>
         </div>
+
         <div className="grid grid-cols-2 gap-1">
+
           <div class="flex flex-col sm:flex-row sm:text-left sm:justify-between">
-            <button class="flex text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Sign Up</button>
+            <Menu as="div" className="relative inline-block text-left">
+              <div>
+                <Menu.Button className="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 hover:bg-gray-50">
+                  Admin
+                  <ChevronDownIcon className="w-5 h-5 -mr-1 text-gray-400" aria-hidden="true" />
+                </Menu.Button>
+              </div>
+
+              <Transition
+                as={Fragment}
+                enter="transition ease-out duration-100"
+                enterFrom="transform opacity-0 scale-95"
+                enterTo="transform opacity-100 scale-100"
+                leave="transition ease-in duration-75"
+                leaveFrom="transform opacity-100 scale-100"
+                leaveTo="transform opacity-0 scale-95"
+              >
+                <Menu.Items className="absolute right-0 z-10 w-56 mt-2 origin-top-right bg-white rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                  <div className="py-1">
+                    <Menu.Item>
+                      {({ active }) => (
+                        <button
+                          type=""
+                          className={classNames(
+                            active ? 'bg-gray-100 text-gray-900' : 'text-gray-700 no-underline',
+                            'block w-full px-4 py-2 text-left text-sm'
+                          )}
+                        >
+                          Signed In as <span class="text-base font-semibold text-blue-700">AdminName</span>
+                        </button>
+                      )}
+                    </Menu.Item>
+                    <form method="POST" action="#">
+                      <Menu.Item>
+                        {({ active }) => (
+                          <button
+                            type="submit"
+                            className={classNames(
+                              active ? 'bg-gray-100 text-gray-900' : 'text-gray-700 no-underline',
+                              'block w-full px-4 py-2 text-left text-sm'
+                            )}
+                          >
+                            Sign out
+                          </button>
+                        )}
+                      </Menu.Item>
+                    </form>
+                  </div>
+                </Menu.Items>
+              </Transition>
+            </Menu>
           </div>
-          <div class="flex flex-col sm:flex-row sm:text-left sm:justify-between">
-            <button class="flex text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Sign In</button>
+          <div class="">
+
           </div>
         </div>
       </nav>
