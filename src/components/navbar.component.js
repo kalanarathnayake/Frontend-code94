5879a25a1a5638d2271e9b3f12c5cbfe152c62f8
kalanarathnayake <kalanagayanga8@gmail.com>
2023-05-04 23:20:35 +0530

    fix: navbar edited

--- src/components/navbar.component.js
+++ src/components/navbar.component.js
@@ -72,7 +72,14 @@ function navbar() {
             </Menu>
           </div>
           <div class="">
-
+            <div class="flex justify-center items-center">
+              <div class="relative">
+                <div class="flex items-center justify-center w-12 h-12 mx-2 overflow-hidden rounded-full">
+                  <img alt='' src="https://images.unsplash.com/photo-1548544149-4835e62ee5b3?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=100&q=80" />
+                </div>
+                <div class="absolute bottom-0 right-0 w-4 h-4 mr-1 rounded-full bg-green-500 border-2 border-white"></div>
+              </div>
+            </div>
           </div>
         </div>
       </nav>
